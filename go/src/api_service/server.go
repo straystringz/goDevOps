ef6da14b870c6abc300aa0f28403f61a7d3cd56f
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2024-04-27 13:12:29 +0100

    Built a little Api service

--- go/src/api_service/server.go
+++ go/src/api_service/server.go
@@ -1,7 +1,45 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/hello-world", handleHelloWorld)
+	http.HandleFunc("/health", handleHealth)
+
+	addr := "localhost:8080"
+	log.Printf("Listening on %s ...", addr)
+
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleHelloWorld(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+	writeReaponse(writer, "Hello World")
+
+}
+
+func handleHealth(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+	writeReaponse(writer, "OK")
+}
+
+func writeReaponse(writer http.ResponseWriter, responseString string) {
+	response := []byte(responseString)
+	fmt.Println(response)
+	_, err := writer.Write(response)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
